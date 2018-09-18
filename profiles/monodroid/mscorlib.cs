7b8cf29d3cc7222c4ce03d07bcd9d155e918db5c
monojenkins <jo.shields+jenkins@xamarin.com>
2018-09-18 12:18:13 +0000

    Apply changes from https://github.com/mono/mono/pull/10617 to API snapshot

--- profiles/monodroid/mscorlib.cs
+++ profiles/monodroid/mscorlib.cs
@@ -25800,3 +25800,32 @@ namespace System.Threading.Tasks
         public override string ToString() { throw null; }
     }
 }
+namespace System.Threading.Tasks.Sources
+{
+    public partial interface IValueTaskSource
+    {
+        void GetResult(short token);
+        System.Threading.Tasks.Sources.ValueTaskSourceStatus GetStatus(short token);
+        void OnCompleted(System.Action<object> continuation, object state, short token, System.Threading.Tasks.Sources.ValueTaskSourceOnCompletedFlags flags);
+    }
+    public partial interface IValueTaskSource<out TResult>
+    {
+        TResult GetResult(short token);
+        System.Threading.Tasks.Sources.ValueTaskSourceStatus GetStatus(short token);
+        void OnCompleted(System.Action<object> continuation, object state, short token, System.Threading.Tasks.Sources.ValueTaskSourceOnCompletedFlags flags);
+    }
+    [System.FlagsAttribute]
+    public enum ValueTaskSourceOnCompletedFlags
+    {
+        FlowExecutionContext = 2,
+        None = 0,
+        UseSchedulingContext = 1,
+    }
+    public enum ValueTaskSourceStatus
+    {
+        Canceled = 3,
+        Faulted = 2,
+        Pending = 0,
+        Succeeded = 1,
+    }
+}
