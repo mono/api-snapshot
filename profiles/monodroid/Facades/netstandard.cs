e7e35d782717de16ad8073a82183f06e8712c7bb
monojenkins <jo.shields+jenkins@xamarin.com>
2018-10-11 16:27:38 +0000

    Apply changes from https://github.com/mono/mono/pull/11084 to API snapshot

--- profiles/monodroid/Facades/netstandard.cs
+++ profiles/monodroid/Facades/netstandard.cs
@@ -711,6 +711,7 @@
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.DivideByZeroException))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.DllNotFoundException))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.Drawing.Color))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.Drawing.Color.ColorType))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.Drawing.Point))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.Drawing.PointF))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedToAttribute(typeof(System.Drawing.Rectangle))]
